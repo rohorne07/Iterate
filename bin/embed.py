82d2223db43e218de8921f9d02e4c37ca9b74117
rohorne07 <55464095+rohorne07@users.noreply.github.com>
2022-01-11 11:42:19 +0000

    Add files via upload

---
+++ bin/embed.py
@@ -0,0 +1,44 @@
+from .jtnnencoder import JTNNEmbed
+
+#%% Test functioning
+smiles = ['C1=CC=C(C=C1)NC2=CC(=CC=C2)O']
+jtnn = JTNNEmbed(smiles)
+
+features = jtnn.get_features()
+
+features
+
+
+#%% Embed dataframe of smiles
+import pandas as pd
+
+df = pd.read_csv('path_to_dir')
+
+df.head()
+df.shape
+lst = list(df['SMILES'])
+
+#process all smiles and featurise
+
+
+smiles = []
+representation = []
+errormol = []
+
+for entry in lst:
+    try:
+        smiles.append(entry)
+        jtnn = JTNNEmbed([entry])
+        features = jtnn.get_features()
+        representation.append(features)
+    except Exception:
+        errormol.append(entry)
+        representation.append('ERROR')
+        continue
+
+
+df['Feature'] = representation
+
+#note the other functions require commma separated values to process, so convert
+#to a list for this purpose
+
